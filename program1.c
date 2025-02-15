586ce0519f88dbb3f9c1f3f9b2399d47b7946caf
Ahlad BVNS <140708572+IAmTheAhladBVNS@users.noreply.github.com>
2025-02-15 14:52:40 +0530

    Update program1.c(Ahlad)

--- program1.c
+++ program1.c
@@ -2,7 +2,7 @@
  * Program to calculate the factorial of a number.
  */
 #include <stdio.h>
-
+#include<stdlib.h>
 int factorial(int n) {
     if (n == 0) return 1;
     return n * factorial(n - 1);
