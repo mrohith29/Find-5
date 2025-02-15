152d88731b8fef104c4c8e5721e4b7541631ed5b
CeLebr-8 <saadhvikreddy2005@gmail.com>
2025-02-15 14:59:52 +0530

    changes

--- program19.c
+++ program19.c
@@ -5,7 +5,7 @@
 
 void addLists(int list1[], int list2[], int result[], int size) {
     for (int i = 0; i < size; i++) {
-        result[i] = list1[i] + list2[i];
+        result[i] = list1[i] - list2[i];
     }
 }
 
