886bb5a965f9733a917ea1179b742ed4765f2355
srikarikuppa <srikarikuppa@gmail.com>
2025-02-15 14:57:27 +0530

    Added ';'

--- program16.c
+++ program16.c
@@ -15,7 +15,7 @@ int main() {
     int x = 10;
     int n = sizeof(arr) / sizeof(arr[0]);
     int result = linearSearch(arr, n, x);
-    (result == -1) ? printf("Element is not present in array")
+    (result == -1) ? printf("Element is not present in array");
                    : printf("Element is present at index %d", result);
     return 0;
 }
